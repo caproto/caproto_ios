06345e045d818d0c53960b88053ad8c100e36441
K Lauer <kenneth.lauer@gmail.com>
2018-10-14 12:35:29 -0700

    Add voice recognition ioc

---
+++ voice_recognition.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import caproto
+from caproto.server import pvproperty, PVGroup, ioc_arg_parser, run
+import speech
+import sound
+
+
+class VoiceRecognitionIOC(PVGroup):
+    duration = pvproperty(value=[5.0],
+                          doc='Duration to listen')
+    start = pvproperty(value=[0],
+                       doc='Start listening')
+    text = pvproperty(value=['text'],
+                      doc='Spoken text',
+                      string_encoding='utf-8',
+                      dtype=caproto.ChannelType.STRING)
+    # TODO: change to CHAR when newer caproto is tagged
+
+    @start.startup
+    async def start(self, instance, async_lib):
+        self.async_lib = async_lib
+
+    @start.putter
+    async def start(self, instance, value):
+        duration, = self.duration.value
+
+        rec = sound.Recorder("recorded.m4a")
+        rec.record()
+        await self.async_lib.sleep(duration)
+        rec.stop()
+        result = speech.recognize("recorded.m4a")
+        print('Heard:', result)
+        await self.text.write([str(result)])
+
+
+if __name__ == '__main__':
+    ioc_options, run_options = ioc_arg_parser(
+        default_prefix='speech:',
+        desc="Spoken word to PV")
+    ioc = VoiceRecognitionIOC(**ioc_options)
+    run(ioc.pvdb, **run_options)
