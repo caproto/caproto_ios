1b69ed1d11f7525e65ffea490658e0b75db68ad8
K Lauer <kenneth.lauer@gmail.com>
2018-10-14 13:12:11 -0700

    Working speech recognition
    Note: write access is required to record audio. This means that it can't
    be done in a "Working Copy" (git repo tool) path directly.  record()
    fails silently when used like this.
    
    Also, of course, microphone + speech recognition permissions are
    required.

--- voice_recognition.py
+++ voice_recognition.py
@@ -10,6 +10,8 @@ class VoiceRecognitionIOC(PVGroup):
                           doc='Duration to listen')
     start = pvproperty(value=[0],
                        doc='Start listening')
+    confidence = pvproperty(value=[0.],
+                            doc='Recognition confidence')
     text = pvproperty(value=['text'],
                       doc='Spoken text',
                       string_encoding='utf-8',
@@ -25,15 +27,24 @@ class VoiceRecognitionIOC(PVGroup):
         duration, = self.duration.value
 
         rec = sound.Recorder('recorded.m4a')
-        rec.record(duration)
-        for i in range(10):
-            print('Recording?', rec.recording)
-            await self.async_lib.sleep(duration / 10.)
+        rec.record()
+        print(f'Recording for {duration}...')
+        await self.async_lib.sleep(duration)
         rec.stop()
         await self.async_lib.sleep(0.1)
         result = speech.recognize('recorded.m4a')
-        print('Heard:', result)
-        await self.text.write([str(result)])
+        if len(result):
+            full_text, full_info = result[0]
+            await self.text.write([full_text])
+            await self.confidence.write([min(info['confidence']
+                                             for info in full_info)])
+            for info in full_info:
+                print('@ {timestamp} {duration} sec'
+                      'heard: {substring} ({confidence})'
+                      ''.format(**info))
+
+        else:
+            await self.text.write([str(result)])
 
 
 if __name__ == '__main__':
