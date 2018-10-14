ee488a4bfab4e0107428a95ab7c3f3b620ebf0f8
K Lauer <kenneth.lauer@gmail.com>
2018-10-14 15:35:24 -0700

    Increase image dimensions

--- camera_ioc.py
+++ camera_ioc.py
@@ -5,23 +5,25 @@ import photos
 import numpy as np
 
 
-image_width, image_height = 1024, 1024
+image_width, image_height = 2048, 2048
 
 
 class CameraIOC(PVGroup):
     acquire = pvproperty(value=[0],
                          doc='Process to acquire an image',
                          mock_record='bo')
+    shape = pvproperty(value=[image_width, image_height],
+                       doc='Image dimensions',
+                       read_only=True)
     image = pvproperty(value=[0] * (image_width * image_height),
-                       doc='Image data')
+                       doc='Image data',
+                       read_only=True)
 
     @acquire.putter
     async def acquire(self, instance, value):
         image = photos.capture_image()
         # resize to (width, height)
-        print(image)
         image = image.resize((image_width, image_height))
-        print(image, np.asarray(image).shape)
         # and convert to grayscale
         image_array = np.dot(np.asarray(image)[..., :3], [0.299, 0.587, 0.114])
         await self.image.write(image_array.flatten().astype(np.uint32))
@@ -29,7 +31,7 @@ class CameraIOC(PVGroup):
 
 if __name__ == '__main__':
     ioc_options, run_options = ioc_arg_parser(
-        default_prefix='motion:',
+        default_prefix='camera:',
         desc="iOS camera IOC")
     ioc = CameraIOC(**ioc_options)
     run(ioc.pvdb, **run_options)
